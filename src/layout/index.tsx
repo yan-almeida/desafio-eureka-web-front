7ac28d081dca0aaeb3caa4d8de7c454a46c3b588
Yan Almeida <yanalmeidagarcia@gmail.com>
2021-08-30 10:22:11 -0300

    refactor: atualização de layout compartilhado

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,14 +1,42 @@
-import { Center, Box, Heading } from '@chakra-ui/react'
+import {
+  Center,
+  Box,
+  Heading,
+  useColorMode,
+  useColorModeValue,
+  VStack,
+  useMediaQuery
+} from '@chakra-ui/react'
 import { WithChildren } from '../../@types/with-children'
+import ThemeToggleButton from '../components/theme-toggle-button'
 
 const Layout = ({ children }: WithChildren) => {
-  return (
-    <Center h="100vh" flexDirection="column" gridGap="10">
-      <Heading mb="8" size="4xl">
-        Buscador de CEPs 📌
-      </Heading>
+  const { colorMode, toggleColorMode } = useColorMode()
+  const [isLargerThan1280] = useMediaQuery('(min-width: 1280px)')
 
-      <Box w="70%">{children}</Box>
+  return (
+    <Center h="100vh" bg={useColorModeValue('gray.100', 'gray.800')}>
+      <Center
+        h="88%"
+        w="93%"
+        border="2px"
+        borderColor={useColorModeValue('blackAlpha.200', 'gray.600')}
+        borderRadius="md"
+        bg={useColorModeValue('whiteAlpha.600', 'gray.800')}
+        flexDirection="column"
+        gridGap="10"
+      >
+        <Heading size={isLargerThan1280 ? '4xl' : '2xl'}>
+          Buscador de CEPs 📌
+        </Heading>
+
+        <Box w={isLargerThan1280 ? '65%' : '90%'}>{children}</Box>
+      </Center>
+
+      <ThemeToggleButton
+        checked={colorMode !== 'light'}
+        toggle={toggleColorMode}
+      />
     </Center>
   )
 }
